69d7f9c1b8674921ba4ff64c231c566b911a4bb7
andreespirela <andreespirela@outlook.com>
2022-07-16 00:16:27 -0400

    Workflow

--- src/cli/cmd/em-wrapper.ts
+++ src/cli/cmd/em-wrapper.ts
@@ -1,6 +1,6 @@
 import {Exm} from "../../common/em";
 
-export const getEmToken = () => process.env.TOKEN || process.env.EXM_TOKEN;
+export const getEmToken = () => process.env.EXM_TOKEN || process.env.TOKEN;
 
 export const em = new Exm({
     token: getEmToken()
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from './common/em';
 export * from './common/model';
 export * from './common/functions/write';
 export * from './common/functions/read';
+export * from './common/functions/deploy';
 export * from './common/exceptions/readFailure';
 export * from './common/exceptions/writeOpFailure';
