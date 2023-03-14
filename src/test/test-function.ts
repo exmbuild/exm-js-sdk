f5e28f4c8b70d00ae2161fae1e8161dff97bfaf0
andreespirela <andreespirela@outlook.com>
2023-03-14 16:21:28 -0600

    0.1.8

--- src/test/test-function.ts
+++ src/test/test-function.ts
@@ -1,4 +1,4 @@
-import {ExecuteConfig, simulateContract, SimulateContractType as ContractType, SimulateInput, Tag} from "three-em-0-3-20";
+import {ExecuteConfig, simulateContract, SimulateContractType as ContractType, SimulateInput, Tag} from "three-em-0-3-21";
 import {guidGenerator} from "../common/utils/commons";
 
 // @ts-ignore
